556af7ca2015de06ed21a4f6dc17e00ecccf843a
oppara <oppara@oppara.tv>
2011-10-01 23:41:45 +0900

    improved build_vim.sh

--- build_vim.sh
+++ build_vim.sh
@@ -1,42 +1,40 @@
 #!/bin/sh
 
 PREFIX="${HOME}/local"
-BRANCH="vim73"
 SRC="${HOME}/src/vim"
 
 ORG_LANG=`echo $LANG`
 export LANG=C
 
 if [ -e ${SRC} ]; then
-  rm -rf ${SRC}
+  cd ${SRC}
+  hg pull -u
+else
+  hg clone https://vim.googlecode.com/hg ${SRC}
+  cd ${SRC}
 fi
-
-hg clone https://vim.googlecode.com/hg ${SRC}
-
-cd ${SRC}
-
-hg update $BRANCH
-hg pull
 
 ./configure --prefix=${PREFIX} \
 --with-features=big \
 --enable-multibyte \
 --enable-xim \
 --enable-fontset \
---enable-rubyinterp \
 --disable-selinux \
 --disable-gui \
+--with-tlib=ncurses \
+--enable-rubyinterp \
 --without-x
 
 make clean
-make
+
+make && cat<<EOT
+------------------------------
+done.
+todo: make install
+EOT
+
 # make install
 
 export LANG="${ORG_LANG}"
 
-cat<<EOT
-------------------------------
-done.
-make install
-EOT
 
